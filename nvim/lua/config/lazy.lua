81e02d0b10a407151748fe5f7acdbea868e0fa92
Maxi Capodacqua <kapomaxi1@gmail.com>
2024-07-09 16:19:09 -0500

    Adds mini-move but only for mac :(

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.editor.mini-move" },
     { import = "plugins" },
   },
   defaults = {
---
+++ nvim/lua/plugins/mini-move.lua
@@ -0,0 +1,18 @@
+return {
+  "echasnovski/mini.move",
+  opts = {
+
+    -- This only works on Mac :(
+    mappings = {
+      left = "˙",
+      right = "¬",
+      down = "∆",
+      up = "˚",
+
+      line_left = "˙",
+      line_right = "¬",
+      line_down = "∆",
+      line_up = "˚",
+    },
+  },
+}
