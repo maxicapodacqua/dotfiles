02d28c41c7f4c1c3f4b4e1843a82bb2c4aa7e8d2
Maxi Capodacqua <kapomaxi1@gmail.com>
2024-07-12 22:17:45 -0500

    Good luck with this one

---
+++ nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,8 @@
+return {
+  "m4xshen/hardtime.nvim",
+  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  opts = {
+    restriction_mode = "hint", -- block or hint
+    disable_mouse = false,
+  },
+}
