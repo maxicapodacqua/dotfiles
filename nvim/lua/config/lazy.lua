4ee70374c5457327b9b1dec09a45a47235af1587
Maxi Capodacqua <kapomaxi1@gmail.com>
2024-07-12 20:22:29 -0500

    Using lazyvim.json to configure extras

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -12,9 +12,9 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.editor.mini-move" },
+    -- { import = "lazyvim.plugins.extras.lang.json" },
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.editor.mini-move" },
     { import = "plugins" },
   },
   defaults = {
