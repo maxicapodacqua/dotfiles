ca4b0c09d952647ee987da381820229f33ceaa4e
Maxi Capodacqua <kapomaxi1@gmail.com>
2024-09-27 12:08:05 -0500

    Using only mini nvim for compatibility with vs code

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,57 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+vim.opt.clipboard = 'unnamedplus'
+-- vim.opt.pumblend = 0
+-- vim.g.autoformat = false
+--
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+	  { 'echasnovski/mini.nvim', version = '*' },
+	  {
+		  "folke/flash.nvim",
+		  event = "VeryLazy",
+		  ---@type Flash.Config
+		  opts = {},
+		  -- stylua: ignore
+		  keys = {
+			  { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+			  { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+			  { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+			  { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+			  { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+		  },
+
+	  }
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
+require("mini.ai").setup()
+require("mini.surround").setup()
