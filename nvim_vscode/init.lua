80505660a65aeeacdb9c4e51136bd6371c0f700b
Maxi Capodacqua <kapomaxi1@gmail.com>
2025-09-04 10:17:51 -0500

    Moving nvim config to nvim_vscode

--- nvim_vscode/init.lua
+++ nvim_vscode/init.lua
@@ -30,21 +30,21 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
 	  { 'echasnovski/mini.nvim', version = '*' },
-	  {
-		  "folke/flash.nvim",
-		  event = "VeryLazy",
-		  ---@type Flash.Config
-		  opts = {},
-		  -- stylua: ignore
-		  keys = {
-			  { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-			  { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-			  { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-			  { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-			  { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-		  },
-
-	  }
+	  -- {
+	  --  "folke/flash.nvim",
+	  --  event = "VeryLazy",
+	  --  ---@type Flash.Config
+	  --  opts = {},
+	  --  -- stylua: ignore
+	  --  keys = {
+	  --   { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+	  --   { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+	  --   { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+	  --   { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+	  --   { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+	  --  },
+	  --
+	  -- }
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
